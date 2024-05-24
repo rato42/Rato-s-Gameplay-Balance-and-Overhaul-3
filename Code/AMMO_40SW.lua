6c67700f330d09e6a3f8613841b0eb2d84f3cabc
Lucjan Dudek <lucekdudek@gmail.com>
2024-05-24 20:49:52 +0200

    .40 S&W

--- Code/AMMO_40SW.lua
+++ Code/AMMO_40SW.lua
@@ -1,167 +1,80 @@
--- ========== _40SW ==========
+-- ========== .40 S&W Designed as a compromise between 9x19mm and .45 ACP ==========
 
 function GBO_40SW()
 	print("GBO _40SW ...")
 
-	_40SW_AP.Cost = 300
-	_40SW_AP.AdditionalHint = T({ "" }) -- TODO
+	_40SW_AP.Cost = 190
+	_40SW_AP.AdditionalHint = T({ "<bullet_point> Improved armor penetration\n<bullet_point> Decreased Critical Damage\n<bullet_point> Increased recoil" })
 	_40SW_AP.Modifications = {
 		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
-			target_prop = "AimAccuracy",
-		}),
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
-			target_prop = "ArmorPir",
-		}),
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
-			target_prop = "CritChance",
-		}),
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
-			target_prop = "CritChanceScaled",
-		}),
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
-			target_prop = "CritDamage",
-		}),
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
-			target_prop = "Noise",
-		}),
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
+			mod_add = 2,
 			target_prop = "PenetrationClass",
 		}),
 		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
-			target_prop = "WeaponRange",
+			mod_add = -20,
+			target_prop = "CritDamage",
+		}),
+		PlaceObj('CaliberModification', {
+			mod_add = 16,
+			target_prop = "Noise",
+		}),
+		PlaceObj('CaliberModification', {
+			mod_add = 1,
+			target_prop = "ArmorPir",
 		}),
 	}
-	_40SW_AP.AppliedEffects = { -- TODO
-	}
 
-	_40SW_Basic.Cost = 150
-	_40SW_Basic.AdditionalHint = T({ "" }) -- TODO
+	_40SW_Basic.Cost = 100
 	_40SW_Basic.Modifications = {
 		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
-			target_prop = "AimAccuracy",
-		}),
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
-			target_prop = "ArmorPir",
-		}),
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
-			target_prop = "CritChance",
-		}),
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
-			target_prop = "CritChanceScaled",
-		}),
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
-			target_prop = "CritDamage",
-		}),
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
+			mod_add = 16,
 			target_prop = "Noise",
 		}),
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
-			target_prop = "PenetrationClass",
-		}),
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
-			target_prop = "WeaponRange",
-		}),
-	}
-	_40SW_Basic.AppliedEffects = { -- TODO
 	}
 
-	_40SW_HP.Cost = 300
-	_40SW_HP.AdditionalHint = T({ "" }) -- TODO
+	_40SW_HP.Cost = 200
+	_40SW_HP.AdditionalHint = T({ "<bullet_point> No armor penetration\n<bullet_point> High Critical Damage\n<bullet_point> Inflicts <color EmStyle>Bleeding</color>" })
 	_40SW_HP.Modifications = {
 		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
-			target_prop = "AimAccuracy",
-		}),
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
-			target_prop = "ArmorPir",
-		}),
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
-			target_prop = "CritChance",
-		}),
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
-			target_prop = "CritChanceScaled",
-		}),
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
+			mod_add = 50,
 			target_prop = "CritDamage",
 		}),
 		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
-			target_prop = "Noise",
-		}),
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
+			mod_add = -4,
 			target_prop = "PenetrationClass",
 		}),
 		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
-			target_prop = "WeaponRange",
+			mod_add = 16,
+			target_prop = "Noise",
 		}),
 	}
-	_40SW_HP.AppliedEffects = { -- TODO
-		"Bleeding"
+	_40SW_HP.AppliedEffects = {
+		"Bleeding",
 	}
 
-	_40SW_Match.Cost = 300
-	_40SW_Match.AdditionalHint = T({ "" }) -- TODO
+	_40SW_Match.Cost = 260
+	_40SW_Match.AdditionalHint = T({ "<bullet_point> Increased bonus from Aiming\n<bullet_point> Increased range\n<bullet_point> Increased Critical Chance scaling" })
 	_40SW_Match.Modifications = {
 		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
+			mod_add = 2,
 			target_prop = "AimAccuracy",
 		}),
 		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
-			target_prop = "ArmorPir",
+			mod_add = 2,
+			target_prop = "WeaponRange",
 		}),
 		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
-			target_prop = "CritChance",
-		}),
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
+			mod_add = 10,
 			target_prop = "CritChanceScaled",
 		}),
 		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
-			target_prop = "CritDamage",
-		}),
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
+			mod_add = 16,
 			target_prop = "Noise",
 		}),
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
-			target_prop = "PenetrationClass",
-		}),
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
-			target_prop = "WeaponRange",
-		}),
 	}
-	_40SW_Match.AppliedEffects = { -- TODO
-	}
+	_40SW_Match.AppliedEffects = {}
 
 	print("GBO _40SW DONE")
 end
 
 OnMsg.ClassesGenerate = GBO_40SW
-
