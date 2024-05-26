2653f281676c366b916824d10bd33edd5315bc3a
Lucjan Dudek <lucekdudek@gmail.com>
2024-05-26 19:00:01 +0200

    WiP Group 5: Assault Rifle and Intermediate Cartridges

--- Code/AMMO_300Blackout.lua
+++ Code/AMMO_300Blackout.lua
@@ -1,166 +1,101 @@
--- ========== _300Blackout ==========
-
+-- ========== .300 Blackout ==========
+-- = Group 5: Assault Rifle and Intermediate Cartridges =
 function GBO_300Blackout()
-	print("GBO _300Blackout ...")
+	print("GBO _300Blackout .300 Blackout ...")
 
-	_300Blackout_AP.Cost = 300
-	_300Blackout_AP.AdditionalHint = T({ "" }) -- TODO
+	_300Blackout_AP.Cost = 400
+	_300Blackout_AP.AdditionalHint = T({
+		"<bullet_point> Improved armor penetration\n<bullet_point> Decreased Critical Damage\n<bullet_point> Increased recoil",
+	})
 	_300Blackout_AP.Modifications = {
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
-			target_prop = "AimAccuracy",
-		}),
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
-			target_prop = "ArmorPir",
-		}),
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
-			target_prop = "CritChance",
-		}),
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
-			target_prop = "CritChanceScaled",
-		}),
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
-			target_prop = "CritDamage",
-		}),
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
-			target_prop = "Noise",
-		}),
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
+		PlaceObj("CaliberModification", {
+			mod_add = 3,
 			target_prop = "PenetrationClass",
 		}),
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
-			target_prop = "WeaponRange",
+		PlaceObj("CaliberModification", {
+			mod_add = -20,
+			target_prop = "CritDamage",
+		}),
+		PlaceObj("CaliberModification", {
+			mod_add = 23,
+			target_prop = "Noise",
+		}),
+		PlaceObj("CaliberModification", {
+			mod_add = 1,
+			target_prop = "ArmorPir",
 		}),
 	}
-	_300Blackout_AP.AppliedEffects = { -- TODO
-	}
+	_300Blackout_AP.AppliedEffects = {}
 
-	_300Blackout_Basic.Cost = 150
-	_300Blackout_Basic.AdditionalHint = T({ "" }) -- TODO
+	_300Blackout_Basic.Cost = 200
+	_300Blackout_Basic.AdditionalHint = T({
+		"",
+	})
 	_300Blackout_Basic.Modifications = {
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
-			target_prop = "AimAccuracy",
-		}),
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
-			target_prop = "ArmorPir",
-		}),
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
-			target_prop = "CritChance",
-		}),
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
-			target_prop = "CritChanceScaled",
-		}),
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
-			target_prop = "CritDamage",
-		}),
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
+		PlaceObj("CaliberModification", {
+			mod_add = 23,
 			target_prop = "Noise",
 		}),
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
+		PlaceObj("CaliberModification", {
+			mod_add = 2,
 			target_prop = "PenetrationClass",
 		}),
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
-			target_prop = "WeaponRange",
-		}),
 	}
-	_300Blackout_Basic.AppliedEffects = { -- TODO
-	}
+	_300Blackout_Basic.AppliedEffects = {}
 
-	_300Blackout_HP.Cost = 300
-	_300Blackout_HP.AdditionalHint = T({ "" }) -- TODO
+	_300Blackout_HP.Cost = 400
+	_300Blackout_HP.AdditionalHint = T({
+		"<bullet_point> No armor penetration\n<bullet_point> High Critical Damage\n<bullet_point> Inflicts <color EmStyle>Bleeding</color>",
+	})
 	_300Blackout_HP.Modifications = {
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
-			target_prop = "AimAccuracy",
-		}),
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
-			target_prop = "ArmorPir",
-		}),
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
-			target_prop = "CritChance",
-		}),
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
-			target_prop = "CritChanceScaled",
-		}),
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
+		PlaceObj("CaliberModification", {
+			mod_add = 50,
 			target_prop = "CritDamage",
 		}),
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
-			target_prop = "Noise",
-		}),
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
+		PlaceObj("CaliberModification", {
+			mod_add = -4,
 			target_prop = "PenetrationClass",
 		}),
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
-			target_prop = "WeaponRange",
+		PlaceObj("CaliberModification", {
+			mod_add = 23,
+			target_prop = "Noise",
 		}),
 	}
-	_300Blackout_HP.AppliedEffects = { -- TODO
-		"Bleeding"
+	_300Blackout_HP.AppliedEffects = {
+		"Bleeding",
 	}
 
-	_300Blackout_Match.Cost = 300
-	_300Blackout_Match.AdditionalHint = T({ "" }) -- TODO
+	_300Blackout_Match.Cost = 400
+	_300Blackout_Match.AdditionalHint = T({
+		"<bullet_point> Increased bonus from Aiming\n<bullet_point> Increased range\n<bullet_point> Increased Critical Chance scaling",
+	})
 	_300Blackout_Match.Modifications = {
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
-			target_prop = "AimAccuracy",
-		}),
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
-			target_prop = "ArmorPir",
-		}),
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
-			target_prop = "CritChance",
-		}),
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
-			target_prop = "CritChanceScaled",
-		}),
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
-			target_prop = "CritDamage",
-		}),
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
-			target_prop = "Noise",
-		}),
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
+		PlaceObj("CaliberModification", {
+			mod_add = 2,
 			target_prop = "PenetrationClass",
 		}),
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
+		PlaceObj("CaliberModification", {
+			mod_add = 10,
+			target_prop = "CritChanceScaled",
+		}),
+		PlaceObj("CaliberModification", {
+			mod_add = 2,
 			target_prop = "WeaponRange",
 		}),
+		PlaceObj("CaliberModification", {
+			mod_add = 23,
+			target_prop = "Noise",
+		}),
+		PlaceObj("CaliberModification", {
+			mod_add = 2,
+			target_prop = "AimAccuracy",
+		}),
 	}
-	_300Blackout_Match.AppliedEffects = { -- TODO
-	}
+	_300Blackout_Match.AppliedEffects = {}
 
-	print("GBO _300Blackout DONE")
+	-- TODO add Subsonic variant
+
+	print("GBO _300Blackout .300 Blackout DONE")
 end
 
 OnMsg.ClassesGenerate = GBO_300Blackout
