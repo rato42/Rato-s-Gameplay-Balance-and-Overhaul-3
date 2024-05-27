659485b32d1e42dfe36196d6da5078961b0b79c8
Lucjan Dudek <lucekdudek@gmail.com>
2024-05-27 14:32:29 +0200

    Group 8: Military Surplus and Obsolete Cartridges

--- Code/AMMO_280British.lua
+++ Code/AMMO_280British.lua
@@ -1,166 +1,97 @@
--- ========== _280British ==========
-
+-- ========== .280 British ==========
+-- = Group 8: Military Surplus and Obsolete Cartridges =
+-- Historically significant, used in older military rifles.
 function GBO_280British()
-	print("GBO _280British ...")
+	print("GBO _280British .280 British ...")
 
-	_280British_AP.Cost = 300
-	_280British_AP.AdditionalHint = T({ "" }) -- TODO
+	_280British_AP.Cost = 200
+	_280British_AP.AdditionalHint = T({
+		"<bullet_point> Improved armor penetration\n<bullet_point> Decreased Critical Damage\n<bullet_point> Increased recoil",
+	})
 	_280British_AP.Modifications = {
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
-			target_prop = "AimAccuracy",
-		}),
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
-			target_prop = "ArmorPir",
-		}),
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
-			target_prop = "CritChance",
-		}),
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
-			target_prop = "CritChanceScaled",
-		}),
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
-			target_prop = "CritDamage",
-		}),
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
-			target_prop = "Noise",
-		}),
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
+		PlaceObj("CaliberModification", {
+			mod_add = 3,
 			target_prop = "PenetrationClass",
 		}),
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
-			target_prop = "WeaponRange",
+		PlaceObj("CaliberModification", {
+			mod_add = -20,
+			target_prop = "CritDamage",
+		}),
+		PlaceObj("CaliberModification", {
+			mod_add = 24,
+			target_prop = "Noise",
+		}),
+		PlaceObj("CaliberModification", {
+			mod_add = 1,
+			target_prop = "ArmorPir",
 		}),
 	}
-	_280British_AP.AppliedEffects = { -- TODO
-	}
+	_280British_AP.AppliedEffects = {}
 
-	_280British_Basic.Cost = 150
-	_280British_Basic.AdditionalHint = T({ "" }) -- TODO
+	_280British_Basic.Cost = 100
 	_280British_Basic.Modifications = {
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
-			target_prop = "AimAccuracy",
-		}),
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
-			target_prop = "ArmorPir",
-		}),
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
-			target_prop = "CritChance",
-		}),
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
-			target_prop = "CritChanceScaled",
-		}),
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
-			target_prop = "CritDamage",
-		}),
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
+		PlaceObj("CaliberModification", {
+			mod_add = 24,
 			target_prop = "Noise",
 		}),
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
+		PlaceObj("CaliberModification", {
+			mod_add = 1,
 			target_prop = "PenetrationClass",
 		}),
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
-			target_prop = "WeaponRange",
-		}),
 	}
-	_280British_Basic.AppliedEffects = { -- TODO
-	}
+	_280British_Basic.AppliedEffects = {}
 
-	_280British_HP.Cost = 300
-	_280British_HP.AdditionalHint = T({ "" }) -- TODO
+	_280British_HP.Cost = 200
+	_280British_HP.AdditionalHint = T({
+		"<bullet_point> No armor penetration\n<bullet_point> High Critical Damage\n<bullet_point> Inflicts <color EmStyle>Bleeding</color>",
+	})
 	_280British_HP.Modifications = {
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
-			target_prop = "AimAccuracy",
-		}),
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
-			target_prop = "ArmorPir",
-		}),
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
-			target_prop = "CritChance",
-		}),
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
-			target_prop = "CritChanceScaled",
-		}),
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
+		PlaceObj("CaliberModification", {
+			mod_add = 50,
 			target_prop = "CritDamage",
 		}),
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
-			target_prop = "Noise",
-		}),
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
+		PlaceObj("CaliberModification", {
+			mod_add = -4,
 			target_prop = "PenetrationClass",
 		}),
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
-			target_prop = "WeaponRange",
+		PlaceObj("CaliberModification", {
+			mod_add = 24,
+			target_prop = "Noise",
 		}),
 	}
-	_280British_HP.AppliedEffects = { -- TODO
-		"Bleeding"
+	_280British_HP.AppliedEffects = {
+		"Bleeding",
 	}
 
-	_280British_Match.Cost = 300
-	_280British_Match.AdditionalHint = T({ "" }) -- TODO
+	_280British_Match.Cost = 200
+	_280British_Match.AdditionalHint = T({
+		"<bullet_point> Increased bonus from Aiming\n<bullet_point> Increased range\n<bullet_point> Increased Critical Chance scaling",
+	})
 	_280British_Match.Modifications = {
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
-			target_prop = "AimAccuracy",
-		}),
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
-			target_prop = "ArmorPir",
-		}),
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
-			target_prop = "CritChance",
-		}),
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
-			target_prop = "CritChanceScaled",
-		}),
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
-			target_prop = "CritDamage",
-		}),
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
-			target_prop = "Noise",
-		}),
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
+		PlaceObj("CaliberModification", {
+			mod_add = 1,
 			target_prop = "PenetrationClass",
 		}),
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
+		PlaceObj("CaliberModification", {
+			mod_add = 10,
+			target_prop = "CritChanceScaled",
+		}),
+		PlaceObj("CaliberModification", {
+			mod_add = 2,
 			target_prop = "WeaponRange",
 		}),
+		PlaceObj("CaliberModification", {
+			mod_add = 24,
+			target_prop = "Noise",
+		}),
+		PlaceObj("CaliberModification", {
+			mod_add = 2,
+			target_prop = "AimAccuracy",
+		}),
 	}
-	_280British_Match.AppliedEffects = { -- TODO
-	}
+	_280British_Match.AppliedEffects = {}
 
-	print("GBO _280British DONE")
+	print("GBO _280British .280 British DONE")
 end
 
 OnMsg.ClassesGenerate = GBO_280British
--- Code/AMMO_303.lua
+++ Code/AMMO_303.lua
@@ -1,166 +1,97 @@
--- ========== _303 ==========
-
+-- ========== .303 British ==========
+-- = Group 8: Military Surplus and Obsolete Cartridges =
+-- Historically significant, used in older military rifles.
 function GBO_303()
-	print("GBO _303 ...")
+	print("GBO _303 .303 British ...")
 
-	_303_AP.Cost = 300
-	_303_AP.AdditionalHint = T({ "" }) -- TODO
+	_303_AP.Cost = 200
+	_303_AP.AdditionalHint = T({
+		"<bullet_point> Improved armor penetration\n<bullet_point> Decreased Critical Damage\n<bullet_point> Increased recoil",
+	})
 	_303_AP.Modifications = {
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
-			target_prop = "AimAccuracy",
-		}),
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
-			target_prop = "ArmorPir",
-		}),
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
-			target_prop = "CritChance",
-		}),
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
-			target_prop = "CritChanceScaled",
-		}),
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
-			target_prop = "CritDamage",
-		}),
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
-			target_prop = "Noise",
-		}),
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
+		PlaceObj("CaliberModification", {
+			mod_add = 3,
 			target_prop = "PenetrationClass",
 		}),
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
-			target_prop = "WeaponRange",
+		PlaceObj("CaliberModification", {
+			mod_add = -20,
+			target_prop = "CritDamage",
+		}),
+		PlaceObj("CaliberModification", {
+			mod_add = 25,
+			target_prop = "Noise",
+		}),
+		PlaceObj("CaliberModification", {
+			mod_add = 1,
+			target_prop = "ArmorPir",
 		}),
 	}
-	_303_AP.AppliedEffects = { -- TODO
-	}
+	_303_AP.AppliedEffects = {}
 
-	_303_Basic.Cost = 150
-	_303_Basic.AdditionalHint = T({ "" }) -- TODO
+	_303_Basic.Cost = 100
 	_303_Basic.Modifications = {
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
-			target_prop = "AimAccuracy",
-		}),
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
-			target_prop = "ArmorPir",
-		}),
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
-			target_prop = "CritChance",
-		}),
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
-			target_prop = "CritChanceScaled",
-		}),
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
-			target_prop = "CritDamage",
-		}),
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
+		PlaceObj("CaliberModification", {
+			mod_add = 25,
 			target_prop = "Noise",
 		}),
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
+		PlaceObj("CaliberModification", {
+			mod_add = 1,
 			target_prop = "PenetrationClass",
 		}),
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
-			target_prop = "WeaponRange",
-		}),
 	}
-	_303_Basic.AppliedEffects = { -- TODO
-	}
+	_303_Basic.AppliedEffects = {}
 
-	_303_HP.Cost = 300
-	_303_HP.AdditionalHint = T({ "" }) -- TODO
+	_303_HP.Cost = 200
+	_303_HP.AdditionalHint = T({
+		"<bullet_point> No armor penetration\n<bullet_point> High Critical Damage\n<bullet_point> Inflicts <color EmStyle>Bleeding</color>",
+	})
 	_303_HP.Modifications = {
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
-			target_prop = "AimAccuracy",
-		}),
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
-			target_prop = "ArmorPir",
-		}),
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
-			target_prop = "CritChance",
-		}),
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
-			target_prop = "CritChanceScaled",
-		}),
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
+		PlaceObj("CaliberModification", {
+			mod_add = 50,
 			target_prop = "CritDamage",
 		}),
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
-			target_prop = "Noise",
-		}),
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
+		PlaceObj("CaliberModification", {
+			mod_add = -4,
 			target_prop = "PenetrationClass",
 		}),
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
-			target_prop = "WeaponRange",
+		PlaceObj("CaliberModification", {
+			mod_add = 25,
+			target_prop = "Noise",
 		}),
 	}
-	_303_HP.AppliedEffects = { -- TODO
-		"Bleeding"
+	_303_HP.AppliedEffects = {
+		"Bleeding",
 	}
 
-	_303_Match.Cost = 300
-	_303_Match.AdditionalHint = T({ "" }) -- TODO
+	_303_Match.Cost = 200
+	_303_Match.AdditionalHint = T({
+		"<bullet_point> Increased bonus from Aiming\n<bullet_point> Increased range\n<bullet_point> Increased Critical Chance scaling",
+	})
 	_303_Match.Modifications = {
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
-			target_prop = "AimAccuracy",
-		}),
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
-			target_prop = "ArmorPir",
-		}),
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
-			target_prop = "CritChance",
-		}),
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
-			target_prop = "CritChanceScaled",
-		}),
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
-			target_prop = "CritDamage",
-		}),
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
-			target_prop = "Noise",
-		}),
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
+		PlaceObj("CaliberModification", {
+			mod_add = 1,
 			target_prop = "PenetrationClass",
 		}),
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
+		PlaceObj("CaliberModification", {
+			mod_add = 10,
+			target_prop = "CritChanceScaled",
+		}),
+		PlaceObj("CaliberModification", {
+			mod_add = 2,
 			target_prop = "WeaponRange",
 		}),
+		PlaceObj("CaliberModification", {
+			mod_add = 25,
+			target_prop = "Noise",
+		}),
+		PlaceObj("CaliberModification", {
+			mod_add = 2,
+			target_prop = "AimAccuracy",
+		}),
 	}
-	_303_Match.AppliedEffects = { -- TODO
-	}
+	_303_Match.AppliedEffects = {}
 
-	print("GBO _303 DONE")
+	print("GBO _303 .303 British DONE")
 end
 
 OnMsg.ClassesGenerate = GBO_303
