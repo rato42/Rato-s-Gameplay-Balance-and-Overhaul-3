c7ddd3261ddb7753fdcd9677d64847a499aec5c5
Lucjan Dudek <lucekdudek@gmail.com>
2024-05-27 14:24:33 +0200

    Group 7: Precision and Long-Range Rifle Cartridges

--- Code/AMMO_300WinMag.lua
+++ Code/AMMO_300WinMag.lua
@@ -1,166 +1,129 @@
--- ========== _300WinMag ==========
-
+-- ========== .300 Winchester Magnum ==========
+-- = Group 7: Precision and Long-Range Rifle Cartridges = 
+-- Long-range, high power.
 function GBO_300WinMag()
-	print("GBO _300WinMag ...")
+	print("GBO _300WinMag .300 Winchester Magnum ...")
 
-	_300WinMag_AP.Cost = 300
-	_300WinMag_AP.AdditionalHint = T({ "" }) -- TODO
+	_300WinMag_AP.Cost = 400
+	_300WinMag_AP.AdditionalHint = T({
+		"<image UI/Conversation/T_Dialogue_IconBackgroundCircle.tga 400 130 128 120> Improved armor penetration\n<image UI/Conversation/T_Dialogue_IconBackgroundCircle.tga 400 130 128 120> Decreased Critical Damage\n<image UI/Conversation/T_Dialogue_IconBackgroundCircle.tga 400 130 128 120> Increased recoil",
+	})
 	_300WinMag_AP.Modifications = {
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
-			target_prop = "AimAccuracy",
-		}),
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
-			target_prop = "ArmorPir",
-		}),
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
-			target_prop = "CritChance",
-		}),
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
-			target_prop = "CritChanceScaled",
-		}),
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
-			target_prop = "CritDamage",
-		}),
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
-			target_prop = "Noise",
-		}),
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
+		PlaceObj("CaliberModification", {
+			mod_add = 4,
 			target_prop = "PenetrationClass",
 		}),
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
+		PlaceObj("CaliberModification", {
+			mod_add = -15,
+			target_prop = "CritDamage",
+		}),
+		PlaceObj("CaliberModification", {
+			mod_add = 26,
+			target_prop = "Noise",
+		}),
+		PlaceObj("CaliberModification", {
+			mod_add = 1,
+			target_prop = "ArmorPir",
+		}),
+		PlaceObj("CaliberModification", {
+			mod_add = 1,
+			target_prop = "AimAccuracy",
+		}),
+		PlaceObj("CaliberModification", {
+			mod_add = 1,
 			target_prop = "WeaponRange",
 		}),
 	}
-	_300WinMag_AP.AppliedEffects = { -- TODO
-	}
+	_300WinMag_AP.AppliedEffects = {}
 
-	_300WinMag_Basic.Cost = 150
-	_300WinMag_Basic.AdditionalHint = T({ "" }) -- TODO
+	_300WinMag_Basic.Cost = 200
 	_300WinMag_Basic.Modifications = {
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
-			target_prop = "AimAccuracy",
-		}),
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
-			target_prop = "ArmorPir",
-		}),
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
-			target_prop = "CritChance",
-		}),
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
-			target_prop = "CritChanceScaled",
-		}),
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
-			target_prop = "CritDamage",
-		}),
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
+		PlaceObj("CaliberModification", {
+			mod_add = 26,
 			target_prop = "Noise",
 		}),
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
+		PlaceObj("CaliberModification", {
+			mod_add = 2,
 			target_prop = "PenetrationClass",
 		}),
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
+		PlaceObj("CaliberModification", {
+			mod_add = 1,
+			target_prop = "AimAccuracy",
+		}),
+		PlaceObj("CaliberModification", {
+			mod_add = 1,
 			target_prop = "WeaponRange",
 		}),
+		PlaceObj("CaliberModification", {
+			mod_add = 5,
+			target_prop = "CritDamage",
+		}),
 	}
-	_300WinMag_Basic.AppliedEffects = { -- TODO
-	}
+	_300WinMag_Basic.AppliedEffects = {}
 
-	_300WinMag_HP.Cost = 300
-	_300WinMag_HP.AdditionalHint = T({ "" }) -- TODO
+	_300WinMag_HP.Cost = 400
+	_300WinMag_HP.AdditionalHint = T({
+		"<image UI/Conversation/T_Dialogue_IconBackgroundCircle.tga 400 130 128 120> No armor penetration\n<image UI/Conversation/T_Dialogue_IconBackgroundCircle.tga 400 130 128 120> High Critical Damage\n<image UI/Conversation/T_Dialogue_IconBackgroundCircle.tga 400 130 128 120> Inflicts <color EmStyle>Bleeding</color> ",
+	})
 	_300WinMag_HP.Modifications = {
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
-			target_prop = "AimAccuracy",
-		}),
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
-			target_prop = "ArmorPir",
-		}),
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
-			target_prop = "CritChance",
-		}),
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
-			target_prop = "CritChanceScaled",
-		}),
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
+		PlaceObj("CaliberModification", {
+			mod_add = 55,
 			target_prop = "CritDamage",
 		}),
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
-			target_prop = "Noise",
-		}),
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
+		PlaceObj("CaliberModification", {
+			mod_add = -4,
 			target_prop = "PenetrationClass",
 		}),
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
+		PlaceObj("CaliberModification", {
+			mod_add = 26,
+			target_prop = "Noise",
+		}),
+		PlaceObj("CaliberModification", {
+			mod_add = 1,
+			target_prop = "AimAccuracy",
+		}),
+		PlaceObj("CaliberModification", {
+			mod_add = 1,
 			target_prop = "WeaponRange",
 		}),
 	}
-	_300WinMag_HP.AppliedEffects = { -- TODO
-		"Bleeding"
+	_300WinMag_HP.AppliedEffects = {
+		"Bleeding",
 	}
 
-	_300WinMag_Match.Cost = 300
-	_300WinMag_Match.AdditionalHint = T({ "" }) -- TODO
+	_300WinMag_Match.Cost = 400
+	_300WinMag_Match.AdditionalHint = T({
+		"<image UI/Conversation/T_Dialogue_IconBackgroundCircle.tga 400 130 128 120> Increased bonus from Aiming\n<image UI/Conversation/T_Dialogue_IconBackgroundCircle.tga 400 130 128 120> Increased range\n<image UI/Conversation/T_Dialogue_IconBackgroundCircle.tga 400 130 128 120> Increased Critical Chance scaling",
+	})
 	_300WinMag_Match.Modifications = {
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
+		PlaceObj("CaliberModification", {
+			mod_add = 3,
 			target_prop = "AimAccuracy",
 		}),
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
-			target_prop = "ArmorPir",
-		}),
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
-			target_prop = "CritChance",
-		}),
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
-			target_prop = "CritChanceScaled",
-		}),
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
-			target_prop = "CritDamage",
-		}),
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
-			target_prop = "Noise",
-		}),
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
-			target_prop = "PenetrationClass",
-		}),
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
+		PlaceObj("CaliberModification", {
+			mod_add = 3,
 			target_prop = "WeaponRange",
 		}),
+		PlaceObj("CaliberModification", {
+			mod_add = 10,
+			target_prop = "CritChanceScaled",
+		}),
+		PlaceObj("CaliberModification", {
+			mod_add = 26,
+			target_prop = "Noise",
+		}),
+		PlaceObj("CaliberModification", {
+			mod_add = 2,
+			target_prop = "PenetrationClass",
+		}),
+		PlaceObj("CaliberModification", {
+			mod_add = 5,
+			target_prop = "CritDamage",
+		}),
 	}
-	_300WinMag_Match.AppliedEffects = { -- TODO
-	}
+	_300WinMag_Match.AppliedEffects = {}
 
-	print("GBO _300WinMag DONE")
+	print("GBO _300WinMag .300 Winchester Magnum DONE")
 end
 
 OnMsg.ClassesGenerate = GBO_300WinMag
--- Code/AMMO_6_5Creedmoor.lua
+++ Code/AMMO_6_5Creedmoor.lua
@@ -1,166 +1,121 @@
--- ========== _6_5Creedmoor ==========
-
+-- ========== 6.5 Creedmoor ==========
+-- = Group 7: Precision and Long-Range Rifle Cartridges = 
+-- Long-range precision, moderate recoil.
 function GBO_6_5Creedmoor()
-	print("GBO _6_5Creedmoor ...")
+	print("GBO _6_5Creedmoor 6.5 Creedmoor ...")
 
-	_6_5Creedmoor_AP.Cost = 300
-	_6_5Creedmoor_AP.AdditionalHint = T({ "" }) -- TODO
+	_6_5Creedmoor_AP.Cost = 350
+	_6_5Creedmoor_AP.AdditionalHint = T({
+		"<image UI/Conversation/T_Dialogue_IconBackgroundCircle.tga 400 130 128 120> Improved armor penetration\n<image UI/Conversation/T_Dialogue_IconBackgroundCircle.tga 400 130 128 120> Decreased Critical Damage\n<image UI/Conversation/T_Dialogue_IconBackgroundCircle.tga 400 130 128 120> Increased recoil",
+	})
 	_6_5Creedmoor_AP.Modifications = {
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
-			target_prop = "AimAccuracy",
-		}),
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
-			target_prop = "ArmorPir",
-		}),
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
-			target_prop = "CritChance",
-		}),
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
-			target_prop = "CritChanceScaled",
-		}),
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
-			target_prop = "CritDamage",
-		}),
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
-			target_prop = "Noise",
-		}),
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
+		PlaceObj("CaliberModification", {
+			mod_add = 4,
 			target_prop = "PenetrationClass",
 		}),
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
+		PlaceObj("CaliberModification", {
+			mod_add = -20,
+			target_prop = "CritDamage",
+		}),
+		PlaceObj("CaliberModification", {
+			mod_add = 25,
+			target_prop = "Noise",
+		}),
+		PlaceObj("CaliberModification", {
+			mod_add = 1,
+			target_prop = "ArmorPir",
+		}),
+		PlaceObj("CaliberModification", {
+			mod_add = 1,
+			target_prop = "AimAccuracy",
+		}),
+		PlaceObj("CaliberModification", {
+			mod_add = 1,
 			target_prop = "WeaponRange",
 		}),
 	}
-	_6_5Creedmoor_AP.AppliedEffects = { -- TODO
-	}
+	_6_5Creedmoor_AP.AppliedEffects = {}
 
-	_6_5Creedmoor_Basic.Cost = 150
-	_6_5Creedmoor_Basic.AdditionalHint = T({ "" }) -- TODO
+	_6_5Creedmoor_Basic.Cost = 180
 	_6_5Creedmoor_Basic.Modifications = {
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
-			target_prop = "AimAccuracy",
-		}),
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
-			target_prop = "ArmorPir",
-		}),
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
-			target_prop = "CritChance",
-		}),
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
-			target_prop = "CritChanceScaled",
-		}),
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
-			target_prop = "CritDamage",
-		}),
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
+		PlaceObj("CaliberModification", {
+			mod_add = 25,
 			target_prop = "Noise",
 		}),
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
+		PlaceObj("CaliberModification", {
+			mod_add = 2,
 			target_prop = "PenetrationClass",
 		}),
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
+		PlaceObj("CaliberModification", {
+			mod_add = 1,
+			target_prop = "AimAccuracy",
+		}),
+		PlaceObj("CaliberModification", {
+			mod_add = 1,
 			target_prop = "WeaponRange",
 		}),
 	}
-	_6_5Creedmoor_Basic.AppliedEffects = { -- TODO
-	}
+	_6_5Creedmoor_Basic.AppliedEffects = {}
 
-	_6_5Creedmoor_HP.Cost = 300
-	_6_5Creedmoor_HP.AdditionalHint = T({ "" }) -- TODO
+	_6_5Creedmoor_HP.Cost = 350
+	_6_5Creedmoor_HP.AdditionalHint = T({
+		"<image UI/Conversation/T_Dialogue_IconBackgroundCircle.tga 400 130 128 120> No armor penetration\n<image UI/Conversation/T_Dialogue_IconBackgroundCircle.tga 400 130 128 120> High Critical Damage\n<image UI/Conversation/T_Dialogue_IconBackgroundCircle.tga 400 130 128 120> Inflicts <color EmStyle>Bleeding</color> ",
+	})
 	_6_5Creedmoor_HP.Modifications = {
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
-			target_prop = "AimAccuracy",
-		}),
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
-			target_prop = "ArmorPir",
-		}),
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
-			target_prop = "CritChance",
-		}),
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
-			target_prop = "CritChanceScaled",
-		}),
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
+		PlaceObj("CaliberModification", {
+			mod_add = 50,
 			target_prop = "CritDamage",
 		}),
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
-			target_prop = "Noise",
-		}),
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
+		PlaceObj("CaliberModification", {
+			mod_add = -4,
 			target_prop = "PenetrationClass",
 		}),
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
+		PlaceObj("CaliberModification", {
+			mod_add = 25,
+			target_prop = "Noise",
+		}),
+		PlaceObj("CaliberModification", {
+			mod_add = 1,
+			target_prop = "AimAccuracy",
+		}),
+		PlaceObj("CaliberModification", {
+			mod_add = 1,
 			target_prop = "WeaponRange",
 		}),
 	}
-	_6_5Creedmoor_HP.AppliedEffects = { -- TODO
-		"Bleeding"
+	_6_5Creedmoor_HP.AppliedEffects = {
+		"Bleeding",
 	}
 
-	_6_5Creedmoor_Match.Cost = 300
-	_6_5Creedmoor_Match.AdditionalHint = T({ "" }) -- TODO
+	_6_5Creedmoor_Match.Cost = 350
+	_6_5Creedmoor_Match.AdditionalHint = T({
+		"<image UI/Conversation/T_Dialogue_IconBackgroundCircle.tga 400 130 128 120> Increased bonus from Aiming\n<image UI/Conversation/T_Dialogue_IconBackgroundCircle.tga 400 130 128 120> Increased range\n<image UI/Conversation/T_Dialogue_IconBackgroundCircle.tga 400 130 128 120> Increased Critical Chance scaling",
+	})
 	_6_5Creedmoor_Match.Modifications = {
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
+		PlaceObj("CaliberModification", {
+			mod_add = 3,
 			target_prop = "AimAccuracy",
 		}),
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
-			target_prop = "ArmorPir",
-		}),
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
-			target_prop = "CritChance",
-		}),
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
-			target_prop = "CritChanceScaled",
-		}),
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
-			target_prop = "CritDamage",
-		}),
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
-			target_prop = "Noise",
-		}),
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
-			target_prop = "PenetrationClass",
-		}),
-		PlaceObj('CaliberModification', {
-			mod_add = 0, -- TODO
+		PlaceObj("CaliberModification", {
+			mod_add = 3,
 			target_prop = "WeaponRange",
 		}),
+		PlaceObj("CaliberModification", {
+			mod_add = 10,
+			target_prop = "CritChanceScaled",
+		}),
+		PlaceObj("CaliberModification", {
+			mod_add = 25,
+			target_prop = "Noise",
+		}),
+		PlaceObj("CaliberModification", {
+			mod_add = 2,
+			target_prop = "PenetrationClass",
+		}),
 	}
-	_6_5Creedmoor_Match.AppliedEffects = { -- TODO
-	}
+	_6_5Creedmoor_Match.AppliedEffects = {}
 
-	print("GBO _6_5Creedmoor DONE")
+	print("GBO _6_5Creedmoor 6.5 Creedmoor DONE")
 end
 
 OnMsg.ClassesGenerate = GBO_6_5Creedmoor
