bfb2eff14ffd5d9889aeae89b377cfda7155646b
wellington-rato <144276750+wellington-rato@users.noreply.github.com>
2024-05-28 00:04:13 -0300

    patch

---
+++ Code/TODO.lua
@@ -0,0 +1,13 @@
+--[[
+	Remove calibers
+	Remove lootdefs, find alternative way
+	remove ammo
+	remove function that drops custom calibers on REACTIONS file
+	change Anaconda to 44 magnum, revise stats
+	some weapons have a purple bullet point
+
+
+	-- zulib
+	append new caliber recoil ratings to the table
+	overwrite tog caliber changes settings, how to?
+]] 
