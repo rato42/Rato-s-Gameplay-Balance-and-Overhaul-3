7294c8a59459e258d6c61b8e0cc3adcd13e9895f
Lucjan Dudek <lucekdudek@gmail.com>
2024-05-31 17:13:21 +0200

    Remove Zulib Weapons Core hard dependency

--- metadata.lua
+++ metadata.lua
@@ -6,12 +6,6 @@ return PlaceObj('ModDef', {
 		"https://www.buymeacoffee.com/rato_modder",
 	},
 	'last_changes', "3.49a -  not released\n\nfixed steyr scout suppresor having no sound",
-	'dependencies', {
-		PlaceObj('ModDependency', {
-			'id', "Tc3ajdY",
-			'title', "Zulib Weapons Core",
-		}),
-	},
 	'id', "cfahRED",
 	'author', "rato",
 	'version_major', 3,
