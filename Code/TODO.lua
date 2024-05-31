fdedabc977a8bb1909ad60b502a8f512e5e5a494
Lucjan Dudek <lucekdudek@gmail.com>
2024-05-31 17:09:32 +0200

    Remove function that drops custom calibers on REACTIONS file

--- Code/TODO.lua
+++ Code/TODO.lua
@@ -1,11 +1,7 @@
 --[[
 	Remove lootdefs, find alternative way
 
-	remove function that drops custom calibers on REACTIONS file
-
 	some weapons have a purple bullet point
-
-	append new caliber recoil ratings to the table
 
     after zulbi next patch:
     - use zCore_ClassesGenerate
